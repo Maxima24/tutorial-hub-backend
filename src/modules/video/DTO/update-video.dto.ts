e6796f0fd4f659d54689161233d278f45827951f
Steel Maxima <steelmaxima21@gmail.com>
2025-12-07 16:57:57 +0100

    revert to previous commit

--- src/modules/video/DTO/update-video.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { UploadVideoSchema } from './upload-video.dto';
-
-export class UpdateVideoSchema extends PartialType(UploadVideoSchema) {}
--- src/modules/video/video.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { VideoService } from './video.service';
-import { VideoController } from './video.controller';
-
-@Module({
-  providers: [VideoService],
-  controllers: [VideoController]
-})
-export class VideoModule {}
