65c4afe00df5f337348f0e7ae01133703fb27352
Steel Maxima <steelmaxima21@gmail.com>
2026-02-03 16:55:31 +0100

    chore:edit prisma service

--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -1,8 +1,28 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import {PrismaClient} from "@prisma/client"
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit,OnModuleDestroy {
+    
+      constructor(private configService: ConfigService) {
+    // Always call super first with the DB URL
+    super({
+      adapter: configService.get<string>('DIRECT_URL'),
+    });
+
+    // Hot reload safety (only in development)
+    if (process.env.NODE_ENV !== 'production') {
+      // @ts-ignore
+      if (!(global as any).prisma) {
+        // @ts-ignore
+        (global as any).prisma = this;
+      } else {
+        // @ts-ignore
+        return (global as any).prisma;
+      }
+    }
+  }
         async onModuleInit() {
             await this.$connect()
         }
